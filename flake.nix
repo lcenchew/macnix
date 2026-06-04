45896d1b97a136fee17d2b4b1507112c19b9a679
lcenchew <lcenchew@outlook.com>
2026-06-04 18:24:44 +0800

    feat: first commit of v2
    A totally revamped flake.nix after accumulating learnings for 2 weeks.

--- flake.nix
+++ flake.nix
@@ -1,20 +1,42 @@
 {
-  description = "A configurable macOS Nix config";
+  description = "A basic configuration of macOS using Nix";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }: {
-    darwinConfigurations."current-host" = nix-darwin.lib.darwinSystem {
-        modules = [ 
-          ./common.nix
-          ./current-host.nix 
-          nix-homebrew.darwinModules.nix-homebrew
-        ];
+  outputs = { self, nix-darwin, nixpkgs }: {
+    # use this to setup:  `sudo -H nix run nix-darwin -- switch --flake .#janmacbook`
+    darwinConfigurations."janmacbook" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ({ pkgs, ... }: {
+          # Define system-wide packages for all users
+          environment.systemPackages = [
+            pkgs.brave
+            pkgs.vscode
+            pkgs.git
+            pkgs.htop
+          ];
+
+          # Allow proprietary packages
+          nixpkgs.config.allowUnfree = true;
+
+          # Set compatibility state version (do not change once deployed)
+          # For latest value, check `system.stateVersion` in 
+          # https://github.com/nix-darwin/nix-darwin/blob/master/modules/system/version.nix
+          system.stateVersion = 7;
+        })
+      ];
     };
+
+    # Allow to pull this file as a template from github
+    # `nix flake init -t "github:lcenchew/macnix"` 
+    templates.default = {
+      path = ./;
+      description = "Template for https://github.com/lcenchew/macnix";
+    }; 
   };
 }
